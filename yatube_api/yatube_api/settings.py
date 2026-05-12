0e2577e82385b8d8351e06c0197f2c9b92cedbe8
unknown <nikitbatyrev@gmail.com>
2026-05-12 17:33:18 +0500

    PEPFINAL

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -90,12 +90,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20, 
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
